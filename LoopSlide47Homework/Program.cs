d0daac54727417ce004c25959cea86c15e981b87
Kendra Egert <Kendra Egert>
2016-09-20 06:12:53 -0400

    first commit

---
+++ LoopSlide47Homework/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopSlide47Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Write a console application that asks the user for an integer. If that integer 
+             * is evenly divisible by 3, then write “You Won!” If it isn’t, write “You Lost.” 
+             * Keep asking them for a number (looping) until they win.
+             */
+                 int counter = 0;
+                 Console.WriteLine("Please enter a number:");
+            do
+            {
+              
+                int LuckyNum = int.Parse(Console.ReadLine());
+                    counter = 0;
+                if (LuckyNum % 3 == 0)
+                {
+                    Console.WriteLine("You won");
+                    Console.ReadLine();
+                    counter = 1001;
+                } else
+                    Console.WriteLine("You Lost.  Please try again.");
+                    Console.WriteLine("Please enter a number:");
+
+            } while (counter < 1001);
+
+
+        }//end main
+    }//end class
+}//end namespce
